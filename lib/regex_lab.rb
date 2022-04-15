8c2689d5a9a33b12b7bedcb0eaf0225670cb580f
antoninaorobets <pani.antonina.don@gmail.com>
2022-04-15 19:43:06 -0400

    commit tree 76020733d2cd338a8880046d266dceffb9aef6e8

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+    res = word.match(/^[aeiou]/i)
+    !!res
+    # binding.pry
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    c= text.scan(/un\w*ing/)
+    #  binding.pry
 end
 
 def words_five_letters_long(text)
-
+   c=  text.scan(/\b[a-zA-Z]{5}\b/)
+    #    binding.pry
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+ c = text.match(/\b[A-Z]\w*[[:punct:]][^\w]/)
+#   binding.pry
 
+!!c
 end
 
 def valid_phone_number?(phone)
+   c= phone.match(/\b[(]?[0-9]{3}[)\s]?[0-9]{3}[-\s]?[0-9]{4}\b/)
+   
+    # binding.pry
 
+!!c
 end
